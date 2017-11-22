49edf7d3f20ebf922c16d624f8b633fb99ce91a0
Maxim <32413334+RedShiftT@users.noreply.github.com>
2017-11-22 15:24:20 +0300

    1

--- index.js
+++ index.js
@@ -174,3 +174,19 @@ http.createServer(function(req, res) {
             }
         });
   }).listen(8080, () => console.log('Все ок'));
+
+// const express = require('express');
+// const passport = require('passport');
+// const session = require('express-session');
+// const RedisStore = require('connect-redis')(session);
+// const app = express();
+// app.use(session({
+//     store: new RedisStore({
+//         url: config.redisStore.url
+//     }),
+//     secret: config.redisStore.secret,
+//     resave: false,
+//     saveUninitialized: false
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
